989af4037b89329a17ee8c9e822fb7651c8a0a96
Anne-françoise STUYK <af.stuyk@afbimservices.fr>
2024-07-12 09:34:24 +0200

    Add Project Organizer

---
+++ TopSolidKernelAutomationExamples/ProjectOrganizer/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace ProjectOrganizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listViewProjects = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btOrganizeProjects = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewProjects
+            // 
+            this.listViewProjects.AllowColumnReorder = true;
+            this.listViewProjects.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listViewProjects.CheckBoxes = true;
+            this.listViewProjects.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewProjects.HideSelection = false;
+            this.listViewProjects.Location = new System.Drawing.Point(21, 29);
+            this.listViewProjects.Name = "listViewProjects";
+            this.listViewProjects.Size = new System.Drawing.Size(416, 255);
+            this.listViewProjects.TabIndex = 0;
+            this.listViewProjects.UseCompatibleStateImageBehavior = false;
+            this.listViewProjects.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Project Name";
+            this.columnHeader1.Width = 135;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Object State";
+            this.columnHeader2.Width = 167;
+            // 
+            // btOrganizeProjects
+            // 
+            this.btOrganizeProjects.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btOrganizeProjects.Location = new System.Drawing.Point(456, 221);
+            this.btOrganizeProjects.Name = "btOrganizeProjects";
+            this.btOrganizeProjects.Size = new System.Drawing.Size(194, 63);
+            this.btOrganizeProjects.TabIndex = 1;
+            this.btOrganizeProjects.Text = "Organize Projects";
+            this.btOrganizeProjects.UseVisualStyleBackColor = true;
+            this.btOrganizeProjects.Click += new System.EventHandler(this.btOrganizeProjects_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 296);
+            this.Controls.Add(this.btOrganizeProjects);
+            this.Controls.Add(this.listViewProjects);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Project Organizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewProjects;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btOrganizeProjects;
+    }
+}
+
---
+++ TopSolidKernelAutomationExamples/ProjectOrganizer/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//TopSolid Automation Kernel
+using TopSolid.Kernel.Automating;
+
+namespace ProjectOrganizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //checks if topsolid automation is connected to perform operation
+            if (TopSolidHost.IsConnected)
+            {
+                //Gets all working projects from current PDM
+                List<PdmObjectId> allWorkingProjects = TopSolidHost.Pdm.GetProjects(true, false);
+
+                ListViewItem.ListViewSubItem[] projectSubItems;
+                ListViewItem projectItem = null;
+
+                List<Tuple<PdmObjectId,string,string>> projectsCompleteData=new List<Tuple<PdmObjectId, string, string>>();
+
+                //retrieve data from projects : name and state - keep id as tag of the item
+                foreach (PdmObjectId project in allWorkingProjects)
+                {
+                    string projectName = TopSolidHost.Pdm.GetName(project);
+                    string projectState = TopSolidHost.Pdm.GetState(project).ToString();
+
+                    projectsCompleteData.Add(Tuple.Create(project, projectName, projectState));                    
+                }
+
+                //then order project list by alphabetical order
+                projectsCompleteData.Sort((x, y) => string.Compare(x.Item2, y.Item2, StringComparison.Ordinal));
+                foreach (Tuple<PdmObjectId, string, string> projectData in projectsCompleteData)
+                {
+                    projectItem = new ListViewItem(projectData.Item2);
+                    projectItem.Tag = projectData.Item1;
+                    projectSubItems = new ListViewItem.ListViewSubItem[]
+                        { new ListViewItem.ListViewSubItem(projectItem, projectData.Item3)};
+                    projectItem.SubItems.AddRange(projectSubItems);
+                    this.listViewProjects.Items.Add(projectItem);
+                }
+            }
+        }
+
+        private void btOrganizeProjects_Click(object sender, EventArgs e)
+        {
+            //if documents are opened, close them
+            TopSolidHost.Documents.CloseAll(true, false);
+
+            for (int i = 0; i < listViewProjects.CheckedItems.Count; i++)
+            {
+                PdmObjectId projectToOrganize = (PdmObjectId)listViewProjects.CheckedItems[i].Tag;
+
+                if (projectToOrganize.IsEmpty) continue;
+
+                //gets all documents from the project
+                Tuple<List<PdmObjectId>, List<PdmObjectId>> documentsAndFolders = GetAllSubDocuments(projectToOrganize);
+
+                //categorize each document according to its type
+                //create a dictionaty to store documents
+                Dictionary<string, List<PdmObjectId>> documentsDictionary = new Dictionary<string, List<PdmObjectId>>();
+
+                //then check documents to fill dictionary
+                foreach (PdmObjectId document in documentsAndFolders.Item1)
+                {
+                    TopSolidHost.Pdm.GetType(document, out string extension);
+                    List<PdmObjectId> result;
+                    if (documentsDictionary.TryGetValue(extension,out result))
+                    {
+                        documentsDictionary[extension].Add(document);
+                    }
+                    else
+                    {
+                        documentsDictionary.Add(extension, new List<PdmObjectId> { document });
+                    }
+                }
+
+                //create one folder per type of document
+                foreach (KeyValuePair<string, List<PdmObjectId>> documentTypeData in documentsDictionary)
+                {
+                    PdmObjectId folderCreated = TopSolidHost.Pdm.CreateFolder(projectToOrganize, documentTypeData.Key.Remove(0, 1));
+
+                    //then move documents
+                    TopSolidHost.Pdm.MoveSeveral(documentTypeData.Value, folderCreated);
+                }
+
+                //clean: delete existing folders (they should be empty right now, but we'll check this anyway)
+                //at first get project folder templates: as they are part of a synchronize group they shouldn't be deleted!
+                TopSolidHost.Pdm.GetProjectTemplates(projectToOrganize, out PdmObjectId outTemplatesFolder, out PdmObjectId outDefaultFolder);
+                List<PdmObjectId> folderstoDelete = new List<PdmObjectId>(documentsAndFolders.Item2);
+                folderstoDelete.Remove(outTemplatesFolder);
+                folderstoDelete.Remove(outDefaultFolder);
+
+                //then delete
+                TopSolidHost.Pdm.DeleteSeveral(folderstoDelete);
+            }
+        }
+
+        /// <summary>
+        /// Private method to get subdocuments and folders from a project
+        /// </summary>
+        /// <param name="inProject">the project To Treat</param>
+        /// <returns>a tuple with flat list of documents and folders</returns>
+        private Tuple<List<PdmObjectId>,List<PdmObjectId>> GetAllSubDocuments(PdmObjectId inProject)
+        {
+            List<PdmObjectId> allDocuments = new List<PdmObjectId>();
+            List<PdmObjectId> allFolders = new List<PdmObjectId>();
+
+            List<PdmObjectId> outFoldersList;
+            List<PdmObjectId> outDocumentList;
+
+            //Get all documents and all folders of the first level
+            TopSolidHost.Pdm.GetConstituents(inProject, out outFoldersList, out outDocumentList);
+
+            allDocuments.AddRange(outDocumentList);
+            allFolders.AddRange(outFoldersList);
+
+            //Redo the method to get all sub sub documents
+            foreach (PdmObjectId objectId in outFoldersList)
+            {
+                allDocuments.AddRange(GetAllSubDocuments(objectId).Item1);
+                allFolders.AddRange(GetAllSubDocuments(objectId).Item2);
+            }
+
+            return Tuple.Create(allDocuments,allFolders);
+        }
+    }
+}
